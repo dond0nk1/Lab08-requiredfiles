708eeef5b5f8c13741ef2ad9e54eb20afb7ad9bb
d0ndonk1 <brandonhiiqingrong@gmail.com>
2025-07-24 17:10:51 +0800

    update eslint with secuirty config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,29 +12,53 @@
 
 // Edited gotten from: https://github.com/jsx-eslint/eslint-plugin-react#configuration
 
-import react from 'eslint-plugin-react';
-import globals from 'globals';
+// import react from 'eslint-plugin-react';
+// import globals from 'globals';
 
+// export default [
+//   {
+//     files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+//     plugins: {
+//       react,
+//     },
+//     languageOptions: {
+//       parserOptions: {
+//         ecmaFeatures: {
+//           jsx: true,
+//         },
+//       },
+//       globals: {
+//         ...globals.browser,
+//       },
+//     },
+//     rules: {
+//       'react/jsx-uses-react': 'error',
+//       'react/jsx-uses-vars': 'error',
+//     },
+//     // ... others are omitted for brevity
+//   },
+// ];
+
+// Updated eslint with security configuration
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginSecurity from "eslint-plugin-security";
+
+/** @type {import('eslint').Linter.Config[]} */
 export default [
+  {files: ["**/*.{js,mjs,cjs,jsx}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
   {
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
     plugins: {
-      react,
-    },
-    languageOptions: {
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-      globals: {
-        ...globals.browser,
-      },
+      security: pluginSecurity
     },
     rules: {
-      'react/jsx-uses-react': 'error',
-      'react/jsx-uses-vars': 'error',
-    },
-    // ... others are omitted for brevity
-  },
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.flat.recommended.rules,
+      "security/detect-eval-with-expression": "error",
+    }
+  }
 ];
